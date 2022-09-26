5ebb3429caede15e3be4d7a059c21f9c7645d0be
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-09-26 08:34:13 +0100

    committed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,27 +7,27 @@ const jwt = require("jsonwebtoken");
 
 const UserController = {
     login: async(req, res) => {
-        const { id, password } = req.body;
-        if (!(id && password)) {
+        const { email, password } = req.body;
+        if (!(email && password)) {
             res.status(400).json("Enter all required inputs");
         };
-        userExists = await db(process.env.DB_T_USERS).select("*").where('id', id)
+        userExists = await db(process.env.DB_T_USERS).select("*").where('email', email)
             .then(row => { return row[0] })
             .catch((err) => { console.log(err); throw err });
 
-        if (userExists || await bcrypt.compare(password, userExists.password)) {
-            const token = jwt.sign({ id },
+        if (userExists && await bcrypt.compare(password, userExists.user_password)) {
+            const token = jwt.sign({ email },
                 process.env.TOKEN_KEY, {
-                    expiresIn: "60d",
+                    expiresIn: "1h",
                 }
             );
             await db(process.env.DB_T_USERS)
-                .where({ id: id })
+                .where({ email: email })
                 .update({ token: token })
                 .then(() => {
                     db(process.env.DB_T_USERS)
-                        .where({ id: id })
-                        .select("id", "first_name", "last_name", "id", "token")
+                        .where({ email: email })
+                        .select("user_id", "first_name", "last_name", "email", "token")
                         .then(response => res.status(200).json({ data: response, message: "user successfully logged in" }))
                         .catch(err => res.status(404).json(err));
                 })
@@ -37,8 +37,8 @@ const UserController = {
         };
     },
     createUser: async(req, res) => {
-        const { first_name, last_name, id, password, confirmPassword, amount, transaction_pin, phone_no } = req.body;
-        if (!(id && password && confirmPassword && first_name && last_name && transaction_pin && phone_no)) {
+        const { first_name, last_name, email, password, confirmPassword, amount, transaction_pin, phone_no } = req.body;
+        if (!(email && password && confirmPassword && first_name && last_name && transaction_pin && phone_no)) {
             return res.status(400).json("Enter all required inputs");
         };
         if (password !== confirmPassword) {
@@ -48,10 +48,10 @@ const UserController = {
             amount = 0;
         };
 
-        userExists = await db(process.env.DB_T_USERS).select("id").where('id', id)
+        userExists = await db(process.env.DB_T_USERS).select("email").where('email', email)
             .then((rows) => {
                 for (row of rows) {
-                    return row['id'];
+                    return row['email'];
                 }
             })
             .catch((err) => { console.log(err); throw err });
@@ -61,13 +61,13 @@ const UserController = {
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const hashedPin = await bcrypt.hash(String(transaction_pin), 10);
-        const token = jwt.sign({ id: id }, process.env.TOKEN_KEY, { expiresIn: "60d" });
+        const token = jwt.sign({ email: email }, process.env.TOKEN_KEY, { expiresIn: "1h" });
         const new_acct_no = await acct_no_gen(db);
 
         await db(process.env.DB_T_USERS).insert({
                 first_name: first_name,
                 last_name: last_name,
-                id: id,
+                email: email,
                 user_password: hashedPassword,
                 account_no: new_acct_no,
                 transaction_pin: hashedPin,
@@ -86,30 +86,30 @@ const UserController = {
 
     getAllUsers: async(_req, res) => {
         await db(process.env.DB_T_USERS)
-            .select("id", "first_name", "last_name", "account_no", "amount", "created_at")
+            .select("user_id", "first_name", "last_name", "account_no", "amount", "created_at")
             .then(response => res.status(200).json(response))
             .catch(err => res.status(400).json(err));
     },
 
     getUser: async(req, res) => {
         await db(process.env.DB_T_USERS)
-            .where({ id: req.params.id })
-            .select("id", "first_name", "last_name", "account_no", "amount", "created_at")
+            .where({ user_id: req.params.id })
+            .select("user_id", "first_name", "last_name", "account_no", "amount", "created_at")
             .then(response => res.status(200).json(response))
             .catch(err => res.status(404).json({ message: "not found", error: err }));
     },
 
     updateUser: async(req, res) => {
-        if (req.body.amount || req.body.created_at || req.body.id || req.body.account_no) {
+        if (req.body.amount || req.body.created_at || req.body.user_id || req.body.account_no) {
             return res.status(403).json("cannot update amount, account number, date created and id");
         }
         await db(process.env.DB_T_USERS)
-            .where({ id: req.params.id })
+            .where({ user_id: req.params.id })
             .update(req.body)
             .then(() => {
                 db(process.env.DB_T_USERS)
-                    .where({ id: req.params.id })
-                    .select("id", "first_name", "last_name", "id", "transaction_pin")
+                    .where({ user_id: req.params.id })
+                    .select("user_id", "first_name", "last_name", "transaction_pin")
                     .then(response => res.status(201).json({ data: response, message: "user successfully updated" }))
                     .catch(err => res.status(404).json(err));
             })
@@ -117,10 +117,10 @@ const UserController = {
     },
 
     deleteUser: async(req, res) => {
-        userExists = await db(process.env.DB_T_USERS).select("id").where('id', req.params.id)
+        userExists = await db(process.env.DB_T_USERS).select("user_id").where('user_id', req.params.id)
             .then((rows) => {
                 for (row of rows) {
-                    return row['id'];
+                    return row['user_id'];
                 }
             })
             .catch((err) => { console.log(err); throw err });
@@ -128,7 +128,7 @@ const UserController = {
             return res.status(400).json("user does not exist");
         }
         await db(process.env.DB_T_USERS)
-            .where({ id: req.params.id })
+            .where({ user_id: req.params.id })
             .del()
             .then(() => res.status(200).json("user successfully deleted"))
             .catch(err => res.status(404).json({ error: err, message: "user not found" }));
